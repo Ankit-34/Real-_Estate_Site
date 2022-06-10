ca8b34714b16c5b2a66a58fded568beef8bdd219
ranjitodedra <ranjit4learning@gmail.com>
2022-06-10 12:12:34 +0530

    backend files added

---
+++ server/model/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	phone: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+});
+
+userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+		expiresIn: "7d",
+	});
+	return token;
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		name: Joi.string().required().label("First Name"),
+		phone: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = { User, validate };
