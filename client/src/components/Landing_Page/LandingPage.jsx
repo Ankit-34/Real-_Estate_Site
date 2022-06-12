91549238d6b75e24d7f8be0242b2bd10cb2d210a
Ankit-34 <ankitganatra007@gmail.com>
2022-06-13 00:56:43 +0530

    setting-up files

--- client/src/components/Landing_Page/LandingPage.jsx
+++ client/src/components/Landing_Page/LandingPage.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar'
 
 const LandPage = () => {
     return<>
          <div className='bg_img'>
-            <div className='navBar'>
-                <ul className='list'>
-                    <li className='item'>Home</li>
-                    <li className='item'>Properties</li>
-                    <li className='item'>Contact Us</li>
-                    <li className='item'>About Us</li>
-                    <li className='item'>Login</li>
-                </ul>
-                <div className='Logo'>Logo</div>
-            </div>
-
+            
+            <Navbar />
 
             <div className='Center'>
                 <p className='Title'>Your <span className='s'>Dream</span> Home</p>
