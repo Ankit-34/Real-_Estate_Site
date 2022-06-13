24b9aa52f3e5dfb018d689be047375d1df23351e
Ankit-34 <ankitganatra007@gmail.com>
2022-06-14 02:42:50 +0530

    Home page Changed

--- client/src/index.js
+++ client/src/index.js
@@ -8,30 +8,50 @@ ReactDOM.render(
     <>
       <LandPage />  
 
+    <div className='Main_header'>
+      
       <div className='Header'>
-        <p className='text_l1'>NEW PROPERTIES</p>
-        <p className='text_l2'>FOR SELL</p>
-        <div className='cards'>
-          <Card />
-          <Card />
-          <Card />
+        <div className='text_l1'>
+          NEW PROPERTIES
+          <br />
+          <span className='text_l2'>FOR SELL</span>
+          <br />
+          <span className='arr'> ➡️ </span>
         </div>
-        <button className='view'>View More</button>
-      </div>
-      <p className='space'></p>
-      <div className='Header'>
-        <p className='text_l1'>NEW PROPERTIES</p>
-        <p className='text_l2'>FOR RENT</p>
-        <div className='cards'>
-          <Card />
-          <Card />
-          <Card />
+        <div className='parent'>
+          <div className='cards'>
+            <Card />
+            <Card />
+            <Card />
+          </div>
+          <button className='view'>View More</button>
         </div>
-        <button className='view'>View More</button>
       </div>
 
-      <div className='Footer'>
-          Footer
+      <p className='space'></p>
+      
+      <div className='Header_'>
+        <div className='parent_'>
+          <div className='cards_'>
+            <Card />
+            <Card />
+            <Card />
+          </div>
+          <button className='view_'>View More</button>
+        </div>
+        
+        <div className='text_l1_'>
+          NEW PROPERTIES
+          <br />
+          <span className='text_l2_'>FOR RENT</span>
+          <br />
+          <span className='arr_'> ⬅️ </span>
+        </div>
       </div>
+    </div>
+
+      {/* <div className='Footer'>
+          Footer
+      </div> */}
        
     </> , document.getElementById('root'));
