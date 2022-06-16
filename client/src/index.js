27af1314a26e3fb509363f543de071908270d0b4
Ankit-34 <ankitganatra007@gmail.com>
2022-06-17 02:38:48 +0530

    Footer added

--- client/src/index.js
+++ client/src/index.js
@@ -32,8 +32,18 @@ ReactDOM.render(
         <FeedBack name="Username 1" />
         <FeedBack name="Username 2" />
         <FeedBack name="Username 3" />
-        <div className='back_shape_right'></div>
+        {/* <div className='back_shape_right'></div> */}
 
+      </div>
+    </div>
+
+    <p className='space'></p>
+    {/* <p className='space'></p> */}
+
+    <div className='Footer'>
+      <div className='box'></div>
+      <div className='footer_image'>
+        <img src='https://media.istockphoto.com/photos/luxurious-beautiful-modern-villa-with-front-yard-garden-at-sunset-picture-id1283532082?b=1&k=20&m=1283532082&s=170667a&w=0&h=KxQ3cfMs-Xi7FL2TXfrgFbi9pwtBOdjSEc4-ufAeVlo=' alt='home_photo'/>
       </div>
     </div>
     
